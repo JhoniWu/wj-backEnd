8f0f987f2d4c110578814a520d08647f58a52493
JhoniWu <1291063373@qq.com>
2023-01-13 23:32:13 +0800

    没啥完善，随便交交

--- src/main/java/com/evan/wj/config/MyWebConfigurer.java
+++ src/main/java/com/evan/wj/config/MyWebConfigurer.java
@@ -3,11 +3,14 @@ package com.evan.wj.config;
 import com.evan.wj.LoginInterceptor.LoginInterceptor;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootConfiguration
+@Configuration
 public class MyWebConfigurer implements WebMvcConfigurer {
 	@Bean
 	public LoginInterceptor getLoginIntercepter(){
@@ -18,4 +21,11 @@ public class MyWebConfigurer implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry){
 		registry.addInterceptor(getLoginIntercepter()).addPathPatterns("/**").excludePathPatterns("/index.html");
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/api/file/**").addResourceLocations("file:" + "E:/JAVA_BackEnd/White-Jotter/img_test/");
+	}
+
+
 }
--- src/main/java/com/evan/wj/controller/LibraryController.java
+++ src/main/java/com/evan/wj/controller/LibraryController.java
@@ -2,9 +2,13 @@ package com.evan.wj.controller;
 
 import com.evan.wj.pojo.Book;
 import com.evan.wj.service.BookService;
+import com.evan.wj.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -52,5 +56,22 @@ public class LibraryController {
 		}
 	}
 
+	@CrossOrigin
+@PostMapping("api/covers")
+public String coversUpload(MultipartFile file) throws Exception {
+    String folder = "E:/JAVA_BackEnd/White-Jotter/img_test";
+		File imageFolder = new File(folder);
+		File f = new File(imageFolder, StringUtils.getRandomString(6) + file.getOriginalFilename().substring(file.getOriginalFilename().length()-4));
+		if(!f.getParentFile().exists())
+			f.getParentFile().mkdirs();
+		try{
+			file.transferTo(f);
+			String imgURL = "http://localhost:8443/api/file/" + f.getName();
+			return imgURL;
+		} catch (IOException e){
+			e.printStackTrace();
+			return "";
+		}
+	}
 
 }
